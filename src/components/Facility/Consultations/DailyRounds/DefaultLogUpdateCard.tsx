465274fb0a9bb67ff0bcdac05a10269e8313cd4b
yash-learner <yash.topaz@gmail.com>
2024-11-13 23:40:09 +0530

    Rename "Daily Rounds" to "Log Updates" in patient log update tests and components

--- src/components/Facility/Consultations/DailyRounds/DefaultLogUpdateCard.tsx
+++ src/components/Facility/Consultations/DailyRounds/DefaultLogUpdateCard.tsx
@@ -26,7 +26,7 @@ const DefaultLogUpdateCard = ({ round, ...props }: Props) => {
   return (
     <div
       className="flex w-full flex-col gap-4 rounded-lg border border-secondary-400 p-4 @container"
-      id="dailyround-entry"
+      id="logupdate-entry"
     >
       <LogUpdateCardAttribute
         attributeKey={"rounds_type"}
