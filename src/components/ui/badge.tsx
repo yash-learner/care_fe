692ecbca85f4febe16f1d06230b0db5a3d9038e3
yash-learner <yash.topaz@gmail.com>
2024-12-18 02:13:20 +0530

    Add new badge variants

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,7 +15,14 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-red-500 text-gray-50 shadow hover:bg-red-500/80 dark:bg-red-900 dark:text-gray-50 dark:hover:bg-red-900/80",
         warning:
-          "border-transparent bg-yellow-400 text-gray-900 shadow hover:bg-yellow-500 dark:bg-yellow-400 dark:text-gray-900 dark:hover:bg-yellow-500",
+          "border-transparent bg-yellow-100 text-gray-900 shadow hover:bg-yellow-500 dark:bg-yellow-400 dark:text-gray-900 dark:hover:bg-yellow-500",
+        info: "border-transparent bg-blue-100 text-blue-900 shadow dark:bg-blue-600 dark:text-gray-50 dark:hover:bg-blue-700",
+        success:
+          "border-transparent bg-green-100 text-white shadow dark:bg-green-600 dark:text-gray-50 dark:hover:bg-green-700",
+        error:
+          "border-transparent bg-red-100 text-red-900 shadow dark:bg-red-700 dark:text-gray-50 dark:hover:bg-red-800",
+        highlight:
+          "border-transparent bg-orange-100 text-orange-900 shadow dark:bg-orange-600 dark:text-gray-50 dark:hover:bg-orange-700",
         outline: "text-gray-950 dark:text-gray-50",
       },
     },
