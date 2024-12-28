0dedb10fe82076fe0d0b3c4407089cce406106c8
Jacobjohnjeevan <mail@jacobjeevan.me>
2024-12-28 23:21:06 +0530

    nav mobile adjustments

--- src/components/ui/sidebar/app-sidebar.tsx
+++ src/components/ui/sidebar/app-sidebar.tsx
@@ -5,13 +5,6 @@ import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
@@ -25,12 +18,12 @@ import {
   PatientNavUser,
 } from "@/components/ui/sidebar/nav-user";
 
-import { Avatar } from "@/components/Common/Avatar";
 import { UserFacilityModel, UserModel } from "@/components/Users/models";
 
 import { PatientUserContextType } from "@/Providers/PatientUserProvider";
-import { classNames } from "@/Utils/utils";
 import { AppointmentPatient } from "@/pages/Patient/Utils";
+
+import { PatientSwitcher } from "./patient-switcher";
 
 const facilityLinks = (selectedFacility: UserFacilityModel | null, t: any) => {
   if (!selectedFacility) {
@@ -139,68 +132,7 @@ export function AppSidebar({
         )}
         {patientUserContext && (
           <>
-            <div
-              className={classNames("mx-2 mt-4 mb-2 flex flex-wrap flex-row")}
-            >
-              <Select
-                disabled={patientUserContext.patients?.length === 0}
-                value={
-                  patientUserContext.selectedPatient
-                    ? patientUserContext.selectedPatient.id
-                    : "Book "
-                }
-                onValueChange={(value) => {
-                  const patient = patientUserContext.patients?.find(
-                    (patient) => patient.id === value,
-                  );
-                  if (patient) {
-                    patientUserContext.setSelectedPatient(patient);
-                    localStorage.setItem(
-                      "selectedPatient",
-                      JSON.stringify(patient),
-                    );
-                  }
-                }}
-              >
-                <SelectTrigger>
-                  <SelectValue
-                    asChild
-                    placeholder={
-                      patientUserContext.patients?.length === 0
-                        ? t("no_patients")
-                        : t("select_patient")
-                    }
-                  >
-                    <div className="flex flex-row justify-between items-center gap-2 w-full text-primary-800">
-                      <Avatar
-                        name={patientUserContext.selectedPatient?.name}
-                        className="h-4 w-4"
-                      />
-                      {
-                        <div className="flex flex-row items-center justify-between w-full gap-2">
-                          <span className="font-semibold truncate max-w-32">
-                            {patientUserContext.selectedPatient?.name}
-                          </span>
-                          <span className="text-xs text-secondary-600">
-                            {t("switch")}
-                          </span>
-                        </div>
-                      }
-                    </div>
-                  </SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  {patientUserContext.patients?.map((patient) => (
-                    <SelectItem key={patient.id} value={patient.id}>
-                      <div className="flex flex-row items-center gap-2">
-                        <Avatar name={patient.name} className="h-4 w-4" />
-                        {patient.name}
-                      </div>
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
+            <PatientSwitcher patientUserContext={patientUserContext} />
             <NavMain
               links={patientLinks(patientUserContext.selectedPatient, t)}
             />
