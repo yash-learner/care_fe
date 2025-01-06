4c3a8a61b759f1801dce9ddd1d09c1c7124c8568
yash-learner <yash.topaz@gmail.com>
2025-01-07 02:13:46 +0530

    Re-wire the sendspecimen

--- src/components/Lab/SendSpecimenForm.tsx
+++ src/components/Lab/SendSpecimenForm.tsx
@@ -12,11 +12,22 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import routes from "@/Utils/request/api";
 import mutate from "@/Utils/request/mutate";
 import { Specimen } from "@/types/emr/specimen";
 
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
 import { BarcodeInput } from "./BarcodeInput";
 
 type SendSpecimenFormProps = {
@@ -28,11 +39,24 @@ const formSchema = z.object({
   lab: z.string().uuid(),
 });
 
+// Mock Labs Data
+const labs = [
+  { id: "b5d3c589-79ad-4c28-a070-bb795ca75ce4", name: "Central Lab" },
+  { id: "e9be65c3-70e7-406c-909d-dffd84501460", name: "Northside Lab" },
+  { id: "20f6958a-d0b4-4fdb-bd3c-1a3bedc728a8", name: "Southside Lab" },
+  { id: "c0b60fd1-cb5e-4265-aab4-03b08f8c69f6", name: "Eastside Lab" },
+  { id: "c713add3-e558-4fb6-a311-dd0c1c3558d5", name: "Westside Lab" },
+];
+
 export const SendSpecimenForm: React.FC<SendSpecimenFormProps> = ({
   onSuccess,
 }) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      barcode: "",
+      lab: "",
+    },
   });
 
   const { mutate: sendSpecimen } = useMutation({
@@ -70,6 +94,48 @@ export const SendSpecimenForm: React.FC<SendSpecimenFormProps> = ({
                   </FormItem>
                 )}
               />
+              <div className="space-y-2">
+                <Label className="text-sm font-normal text-gray-900">
+                  Select Lab
+                </Label>
+                <FormField
+                  control={form.control}
+                  name="lab"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Select
+                          value={field.value}
+                          onValueChange={field.onChange}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a Lab" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectGroup>
+                              <SelectLabel>Labs</SelectLabel>
+                              {labs.map((lab) => (
+                                <SelectItem key={lab.id} value={lab.id}>
+                                  {lab.name}
+                                </SelectItem>
+                              ))}
+                            </SelectGroup>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button
+                type="submit"
+                variant={"primary"}
+                size={"lg"}
+                className="self-end"
+              >
+                Save
+              </Button>
             </form>
           </Form>
         </div>
